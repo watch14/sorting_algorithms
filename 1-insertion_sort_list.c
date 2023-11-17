cd239531a09bf4510ee01de371462a0fbefd45af
Iheb Chaoued <ihebchaoued@gmail.com>
2023-11-17 13:12:57 +0000

    test

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,17 +1,24 @@
 #include "sort.h"
 
-
-
+/**
+ * insertion_sort_list - Sorts a doubly linked list.
+ *
+ * @list: Pointer to a pointer to the head of the linked list.
+ *
+ * Description: The function sorts the doubly linked list in place by swapping
+ * nodes, and prints the list after each swap.
+ */
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *now = (*list)->next;
-	listint_t *before = now->previous;
 
 	if (list == NULL || (*list)->next == NULL || *list == NULL)
 		return;
 
 	while (now != NULL)
 	{
+		listint_t *before = now->previous;
+
 		while (before != NULL && before->n > now->n)
 		{
 			if (before->previous != NULL)
