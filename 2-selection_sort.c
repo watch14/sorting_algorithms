f4956730304bed86e8cdf136de39f5a8022f0a53
watch14 <watchman1414@gmail.com>
2023-11-18 10:42:14 +0000

    fast_push

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,40 +1,26 @@
 #include "sort.h"
 
 /**
+ * swap - Swaps the values of two integers.
+ * @xp: Pointer to the first integer.
+ * @yp: Pointer to the second integer.
+ *
+ * Return: void
+ */
+
+void swap(int *xp, int *yp)
+{
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+/**
  * selection_sort - Sorts an array of integers in ascending order.
  *
  * @array: The array to be sorted.
  * @size: The size of the array.
  */
-void selection_sort(int *array, size_t size)
-{
-	size_t i, j, min_index;
-
-	int temp
-
-	if (array == NULL || size < 2)
-		return;
-
-	for (i = 0; i < size - 1; i++)
-	{
-		min_index = i;
-		for (j = i + 1; j < size; j++)
-		{
-			if (array[j] < array[min_index])
-				min_index = j;
-		}
-
-		if (min_index != i)
-		{
-			temp = array[i];
-			array[i] = array[min_index];
-			array[min_index] = temp;
-
-			print_array(array, size);
-		}
-	}
-}
-
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min;
