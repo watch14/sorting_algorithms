0ed9efeff801400c645345c09c6255fc1eb12489
watch14 <watchman1414@gmail.com>
2023-11-18 11:41:06 +0000

    fast_push

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -12,3 +12,34 @@ void swap(int *x, int *y)
 	*x = *y;
 	*y = temp;
 }
+
+/**
+ * quick_sort - Sorts an array in ascending order using Quick Sort
+ * @array: The array to be sorted.
+ * @size: The size of the array.
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int flag;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0 ; i < size  ; i++)
+	{
+		flag = 0;
+		for (j = size - 1 ; j > i ; j--)
+		{
+
+			if (array[i] > array[j])
+			{
+				swap(&array[i], &array[j]);
+				flag = 1;
+			}
+		}
+		if (flag == 1)
+			print_array(array, size);
+	}
+}
