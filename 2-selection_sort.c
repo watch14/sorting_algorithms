5a50b94d01688a980923184aa1782da556b1f3ef
watch14 <watchman1414@gmail.com>
2023-11-18 10:43:10 +0000

    fast_push

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -31,11 +31,11 @@ void selection_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		min = i;
-		for (j = i+1; j < size; j++)
+		for (j = i + 1; j < size; j++)
 			if (array[j] < array[min])
 				min = j;
 
-		if(min != i)
+		if (min != i)
 			swap(&array[min], &array[i]);
 		print_array(array, size);
 	}
