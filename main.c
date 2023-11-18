a7a6fc51c3f3f0e0ec665ea051b9f071cfa50a30
Iheb Chaoued <ihebchaoued@gmail.com>
2023-11-18 15:27:34 +0000

    main

--- main.c
+++ main.c
@@ -2,6 +2,28 @@
 #include <stdlib.h>
 #include "sort.h"
 
+listint_t *create_listint(const int *array, size_t size)
+{
+	listint_t *list;
+	listint_t *node;
+	int *tmp;
+
+	list = NULL;
+	while (size--)
+	{
+		node = malloc(sizeof(*node));
+		if (!node)
+			return (NULL);
+		tmp = (int *)&node->n;
+		*tmp = array[size];
+		node->next = list;
+		node->prev = NULL;
+		list = node;
+		if (list->next)
+			list->next->prev = list;
+	}
+	return (list);
+}
 void print_array(const int *array, size_t size)
 {
 	size_t i;
@@ -40,13 +62,17 @@ void print_list(const listint_t *list)
  */
 int main(void)
 {
+	listint_t *list;
 	int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
 	size_t n = sizeof(array) / sizeof(array[0]);
 
-	print_array(array, n);
+	list = create_listint(array, n);
+	if (!list)
+		return (1);
+	print_list(list);
 	printf("\n");
-	bubble_sort(array, n);
+	insertion_sort_list(&list);
 	printf("\n");
-	print_array(array, n);
+	print_list(list);
 	return (0);
 }
