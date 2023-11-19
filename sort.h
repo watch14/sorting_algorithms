3fe1d9b4212c4933555245e3ed5b009c5a7d295a
Iheb Chaoued <ihebchaoued@gmail.com>
2023-11-19 21:20:00 +0000

    lib

--- sort.h
+++ sort.h
@@ -30,5 +30,7 @@ void quick_sort(int *array, size_t size);
 
 /* ADVANCED */
 void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 
 #endif /* SORT_H */
