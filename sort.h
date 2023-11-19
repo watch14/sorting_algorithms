a3be67c1af8a655bf1b6136b3c778a90ee73cc53
Iheb Chaoued <ihebchaoued@gmail.com>
2023-11-19 21:16:32 +0000

    lib

--- sort.h
+++ sort.h
@@ -29,6 +29,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
 /* ADVANCED */
-void shell_sort(int *array, size_t size)
+void shell_sort(int *array, size_t size);
 
 #endif /* SORT_H */
