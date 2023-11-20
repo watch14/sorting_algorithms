85eb6f9951220f5c01bb6ea6f74410f82d938781
watch14 <watchman1414@gmail.com>
2023-11-20 15:12:13 +0000

    fast_push

--- saved/sort.h
+++ saved/sort.h
@@ -1,5 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
+
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -22,10 +23,11 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
 
-
-
-
-
-#endif
+#endif /* SORT_H */
