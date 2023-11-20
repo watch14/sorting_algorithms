23816747259ed3c0bfed2ed5589ad2e23da2f3b9
watch14 <watchman1414@gmail.com>
2023-11-20 15:08:22 +0000

    fast_push

--- saved/sort.h
+++ saved/sort.h
@@ -1,9 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
-
-#include <stdlib.h>
+#include <stdbool.h>
 #include <stdio.h>
-#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -19,15 +17,15 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
 
-void shell_sort(int *array, size_t size);
-void swap_nodes(listint_t **list, listint_t *node);
+
+
+
+
 
 #endif
