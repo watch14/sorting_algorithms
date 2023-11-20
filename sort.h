e444a8d9da1c0929b62c053dc26a00cdafbb88b5
Iheb Chaoued <ihebchaoued@gmail.com>
2023-11-20 12:17:32 +0000

    lib

--- sort.h
+++ sort.h
@@ -36,4 +36,10 @@ void quick_sort(int *array, size_t size);
 int lomuto_partition(int *array, int low, int high, size_t size);
 void quicksort(int *array, int low, int high, size_t size);
 
+
+/* ADVANCED */
+
+/* TASK 100 */
+void shell_sort(int *array, size_t size);
+
 #endif /* SORT_H */
