60949e77c517bd9ccd13b29d8d7e2f9d207f4e7c
watch14 <watchman1414@gmail.com>
2023-11-20 12:57:51 +0000

    fast_push

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -68,11 +68,11 @@ void quick_sort_recursive(int *array, int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
+	int low = 0;
+        int high = size - 1;
+
 	if (array == NULL || size < 2)
 		return;
-
-	int low = 0;
-	int high = size - 1;
 
 	quick_sort_recursive(array, low, high, size);
 }
